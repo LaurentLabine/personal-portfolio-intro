57beb2c0a94abd3255562b13e582e156934ee2e0
Laurent Labine <laurentlabine@gmail.com>
2021-05-20 11:11:18 +0200

    Centered the All rights reserved in the footer.

--- gatsby-theme-intro/src/components/footer/footer.js
+++ gatsby-theme-intro/src/components/footer/footer.js
@@ -4,8 +4,8 @@ import { ProfileType } from "../../types"
 
 const Footer = ({ name, showThemeLogo = true }) => (
   <footer className="bg-front mt-16 pt-8 pb-16">
-    <div className="md:max-w-screen-sm lg:max-w-screen-xl mx-auto px-4 flex items-center">
-      <div className="w-2/3 text-back-light font-header text-xs">
+    <div className="md:max-w-screen-sm lg:max-w-screen-xl mx-auto px-4 flex justify-center items-center">
+      <div className="text-center text-back-light font-header text-m">
         <b>
           &copy; {new Date().getFullYear()} {name}.
         </b>{" "}
